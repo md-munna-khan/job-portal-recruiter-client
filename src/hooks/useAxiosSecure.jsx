62d7e52164fd47c12ba013a1dce473c7b747290a
md.munna <mk8761174@gmail.com>
2024-12-17 17:53:02 +0600

    create job recruiter

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+const axiosInstance = axios.create({
+    baseURL:'http://localhost:5000',
+    withCredentials:true
+})
+const useAxiosSecure = () => {
+const { signOutUser}=useAuth()
+const navigate = useNavigate()
+    useEffect(()=>{
+        axiosInstance.interceptors.response.use(response=>{
+            return response
+        },error=>{
+            console.log('error caught in interceptor', error);
+          if(error.status === 401|| error.status === 403){
+            console.log('need to log out user')
+            signOutUser()
+            .then(()=>{
+                console.log('logged out user')
+                navigate('/signIn');
+            })
+            .catch((error)=> console.log(error))
+          }
+            return Promise.reject(error)
+        }
+    )
+    },[])
+    return axiosInstance
+};
+
+export default useAxiosSecure;
